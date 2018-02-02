b21aef50b8df170a9f46415594a3ff0e084e2ac7
Ramesh Sivaraman <ramesh.sivaraman@percona.com>
2018-02-02 13:31:49 +0530

    PQA-162 : Add additional BATS testcases to ProxySQL testing

---
+++ tests/proxysql-admin-testsuite.bats
@@ -0,0 +1,52 @@
+## proxysql-admin setup tests
+source /etc/proxysql-admin.cnf
+
+@test "run proxysql-admin -d" {
+run sudo proxysql-admin -d
+echo "$output"
+    [ "$status" -eq  0 ]
+}
+
+@test "run proxysql-admin -e" {
+run sudo proxysql-admin -e <<< 'n'
+echo "$output"
+    [ "$status" -eq  0 ]
+}
+
+@test "run the check for cluster size" {
+  #get values from PXC and ProxySQL side
+  wsrep_cluster_count=$(mysql --user=$CLUSTER_APP_USERNAME --password=$CLUSTER_APP_PASSWORD  --host=$CLUSTER_HOSTNAME --port=6033 --protocol=tcp -Bse"show status like 'wsrep_cluster_size'" | awk '{print $2}')
+  proxysql_cluster_count=$(mysql --user=$PROXYSQL_USERNAME --password=$PROXYSQL_PASSWORD --host=$PROXYSQL_HOSTNAME --port=$PROXYSQL_PORT --protocol=tcp -Bse "select count(*) from mysql_servers" | awk '{print $0}')
+  [ "$wsrep_cluster_count" -eq "$proxysql_cluster_count" ]
+}
+
+@test "run the check for --node-check-interval" {
+  report_interval=$(mysql --user=$PROXYSQL_USERNAME -p$PROXYSQL_PASSWORD  --host=$PROXYSQL_HOSTNAME --port=$PROXYSQL_PORT --protocol=tcp -Bse"select interval_ms from scheduler where id=10" | awk '{print $0}')
+  [ "$report_interval" -eq 3000 ]
+}
+
+@test "run the check for --adduser" {
+  run_add_command=$(printf "proxysql_test_user1\ntest_user\ny" | sudo proxysql-admin --adduser)
+  run_check_user_command=$(mysql --user=$PROXYSQL_USERNAME -p$PROXYSQL_PASSWORD  --host=$PROXYSQL_HOSTNAME --port=$PROXYSQL_PORT --protocol=tcp -Bse "select 1 from mysql_users where username='proxysql_test_user1'" | awk '{print $0}')
+  [ "$run_check_user_command" -eq 1 ]
+}
+
+@test "run proxysql-admin --syncusers" {
+run sudo proxysql-admin --syncusers
+echo "$output"
+    [ "$status" -eq  0 ]
+}
+
+@test "run the check for --syncusers" {
+  cluster_user_count=$(mysql --user=$CLUSTER_USERNAME --password=$CLUSTER_PASSWORD  --host=$CLUSTER_HOSTNAME --port=$CLUSTER_PORT --protocol=tcp -Bse"select count(*) from mysql.user where authentication_string!='' and user not in ('admin','mysql.sys')" | awk '{print $1}')
+  proxysql_user_count=$(mysql --user=$PROXYSQL_USERNAME -p$PROXYSQL_PASSWORD  --host=$PROXYSQL_HOSTNAME --port=$PROXYSQL_PORT --protocol=tcp -Bse "select count(*) from mysql_users" | awk '{print $0}')
+  [ "$cluster_user_count" -eq "$proxysql_user_count" ]
+}
+
+
+@test "run the check for --test-run" {
+  run sudo proxysql-admin  --enable --quick-demo <<< n
+  echo "$output"
+    [ "$status" -eq 1 ]
+    [ "${lines[3]}" = "You have selected No. Terminating." ]
+}
---
+++ tests/proxysql-admin-testsuite.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+# Created by Ramesh Sivaraman, Percona LLC
+# The script is used for testing proxysql-admin functionality
+
+# User Configurable Variables
+WORKDIR=$1
+SBENCH="sysbench"
+SCRIPT_PWD=$(cd `dirname $0` && pwd)
+PXC_START_TIMEOUT=200
+WORKDIR="${WORKDIR}/$PROXYSQL_BASE_NUMBER"
+RPORT=$(( RANDOM%21 + 10 ))
+RBASE="$(( RPORT*1000 ))"
+ADDR="127.0.0.1"
+SUSER=root
+SPASS=
+
+if [ -z $WORKDIR ];then
+  WORKDIR="${PWD}"
+fi 
+ROOT_FS=$WORKDIR
+
+mkdir -p $WORKDIR/logs
+
+ps -ef | egrep "mysqld" | grep "$(whoami)" | egrep -v "grep" | xargs kill -9 2>/dev/null
+ps -ef | egrep "proxysql" | grep "$(whoami)" | egrep -v "grep" | xargs kill -9 2>/dev/null
+
+cd ${WORKDIR}
+
+#Check PXC binary tar ball
+PXC_TAR=$(ls -1td ?ercona-?tra??-?luster* | grep ".tar" | head -n1)
+if [ ! -z $PXC_TAR ];then
+  tar -xzf $PXC_TAR
+  PXCBASE=$(ls -1td ?ercona-?tra??-?luster* | grep -v ".tar" | head -n1)
+  export PATH="$WORKDIR/$PXCBASE/bin:$PATH"
+  PXC_BASEDIR="${WORKDIR}/$PXCBASE"
+else
+  echo "ERROR! Percona-XtraDB-Cluster binary tarball does not exist. Terminating"
+  exit 1
+fi
+
+
+PROXYSQL_TAR=$(ls -1td proxysql-*.tar.gz | grep ".tar" | head -n1)
+if [ ! -z $PROXYSQL_TAR ];then
+  tar -xzf $PROXYSQL_TAR
+  PROXYSQL_BASE=$(ls -1td proxysql-1* | grep -v ".tar" | head -n1)
+  export PATH="$WORKDIR/$PXCBASE/usr/bin/:$PATH"
+  PROXYSQL_BASE="${WORKDIR}/$PROXYSQL_BASE"
+else
+  echo "ERROR! proxysql binary tarball does not exist. Terminating"
+  exit 1
+fi
+
+rm *.tar.gz
+$PROXYSQL_BASE/usr/bin/proxysql -D $PROXYSQL_BASE  $PROXYSQL_BASE/proxysql.log &
+
+
+if [ "$(${PXC_BASEDIR}/bin/mysqld --version | grep -oe '5\.[567]' | head -n1)" == "5.7" ]; then
+  MID="${PXC_BASEDIR}/bin/mysqld --no-defaults --initialize-insecure --basedir=${PXC_BASEDIR}"
+elif [ "$(${PXC_BASEDIR}/bin/mysqld --version | grep -oe '5\.[567]' | head -n1)" == "5.6" ]; then
+  MID="${PXC_BASEDIR}/scripts/mysql_install_db --no-defaults --basedir=${PXC_BASEDIR}"
+fi
+
+
+start_pxc_node(){
+  NODES=3
+  # Creating default my.cnf file
+  cd $PXC_BASEDIR
+  echo "[mysqld]" > my.cnf
+  echo "basedir=${PXC_BASEDIR}" >> my.cnf
+  echo "innodb_file_per_table" >> my.cnf
+  echo "innodb_autoinc_lock_mode=2" >> my.cnf
+  echo "innodb_locks_unsafe_for_binlog=1" >> my.cnf
+  echo "wsrep-provider=${PXC_BASEDIR}/lib/libgalera_smm.so" >> my.cnf
+  echo "wsrep_node_incoming_address=$ADDR" >> my.cnf
+  echo "wsrep_sst_method=rsync" >> my.cnf
+  echo "wsrep_sst_auth=$SUSER:$SPASS" >> my.cnf
+  echo "wsrep_node_address=$ADDR" >> my.cnf
+  echo "core-file" >> my.cnf
+  echo "log-output=none" >> my.cnf
+  echo "server-id=1" >> my.cnf
+  echo "wsrep_slave_threads=2" >> my.cnf
+
+  for i in `seq 1 $NODES`;do
+    RBASE1="$(( RBASE + ( 100 * $i ) ))"
+    LADDR1="$ADDR:$(( RBASE1 + 8 ))"
+    WSREP_CLUSTER="${WSREP_CLUSTER}gcomm://$LADDR1,"
+    node="${PXC_BASEDIR}/node$i"
+    if [ "$(${PXC_BASEDIR}/bin/mysqld --version | grep -oe '5\.[567]' | head -n1 )" != "5.7" ]; then
+      mkdir -p $node $keyring_node
+      if  [ ! "$(ls -A $node)" ]; then 
+        ${MID} --datadir=$node  > $WORKDIR/logs/startup_node$i.err 2>&1 || exit 1;
+      fi
+    fi
+    if [ ! -d $node ]; then
+      ${MID} --datadir=$node  > $WORKDIR/logs/startup_node$i.err 2>&1 || exit 1;
+    fi
+    if [ $i -eq 1 ]; then
+      WSREP_CLUSTER_ADD="--wsrep_cluster_address=gcomm:// "
+	  BASEPORT=$RBASE1
+    else
+      WSREP_CLUSTER_ADD="--wsrep_cluster_address=$WSREP_CLUSTER"
+    fi
+
+    ${PXC_BASEDIR}/bin/mysqld --defaults-file=${PXC_BASEDIR}/my.cnf \
+      --datadir=$node $WSREP_CLUSTER_ADD \
+      --wsrep_provider_options=gmcast.listen_addr=tcp://$LADDR1 \
+      --log-error=$WORKDIR/logs/node$i.err \
+      --socket=$node/socket.sock --port=$RBASE1 > $WORKDIR/logs/node$i.err 2>&1 &
+    for X in $(seq 0 ${PXC_START_TIMEOUT}); do
+      sleep 1
+      if ${PXC_BASEDIR}/bin/mysqladmin -uroot -S$node/socket.sock ping > /dev/null 2>&1; then
+        echo "Started PXC node$i. Socket : $node/socket.sock"
+        break
+      fi
+    done
+  done
+}
+
+start_pxc_node
+
+${PXC_BASEDIR}/bin/mysql -uroot -S${PXC_BASEDIR}/node1/socket.sock -e"GRANT ALL ON *.* TO admin@'%' identified by 'admin';flush privileges;"
+sed -i "s/3306/${BASEPORT}/" $PROXYSQL_BASE/etc/proxysql-admin.cnf
+sudo cp $PROXYSQL_BASE/etc/proxysql-admin.cnf /etc/proxysql-admin.cnf
+sudo cp $PROXYSQL_BASE/usr/bin/* /usr/bin/
+ 
+if [[ ! -e $(which bats 2> /dev/null) ]] ;then
+  pushd $ROOT_FS
+  git clone https://github.com/sstephenson/bats
+  cd bats
+  sudo ./install.sh /usr/local
+  popd
+fi
+
+echo "proxysql-admin generic bats test log"
+sudo TERM=xterm bats $SCRIPT_PWD/generic-test.bats 
+echo "proxysql-admin testsuite bats test log"
+sudo TERM=xterm bats $SCRIPT_PWD/proxysql-admin-testsuite.bats 
+
+${PXC_BASEDIR}/bin/mysqladmin  --socket=${PXC_BASEDIR}/node1/socket.sock  -u root shutdown
+${PXC_BASEDIR}/bin/mysqladmin  --socket=${PXC_BASEDIR}/node2/socket.sock  -u root shutdown
+${PXC_BASEDIR}/bin/mysqladmin  --socket=${PXC_BASEDIR}/node3/socket.sock  -u root shutdown
